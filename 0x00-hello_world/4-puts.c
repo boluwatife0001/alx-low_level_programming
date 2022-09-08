08cef784297c2fa964019caf1c67397220ff4f4a
boluwatife0001 <111499004+boluwatife0001@users.noreply.github.com>
2022-09-08 17:07:44 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Description: A C program that prints with printf function.
+ * Description: A C program that prints with put function
  *
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
